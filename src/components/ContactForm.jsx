1247754e14e17dbf59d36cc44c55d55fcbcd18f4
Arash Falah Parsa <arash_afp_94@hotmail.com>
2024-01-25 19:58:28 +0100

    edited fullname to name on label id

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -5,8 +5,8 @@ const ContactForm = ({ className }) => {
     <div className={`text-black ${className}`}>
       <form className="flex flex-col border-2 border-black rounded-3xl px-10 pb-10 pt-2 bg-blue-950 gap-2">
         <h1 className="text-white text-center underline font-bold text-2xl"> Contact Us</h1>
-        <label id="fullName"  htmlFor="text" className="text-white">Full Name:</label>
-        <input id="fullName" type="text" className="rounded-lg px-2 py-1" placeholder="John Doe" />
+        <label id="name"  htmlFor="text" className="text-white">Full Name:</label>
+        <input id="name" type="text" className="rounded-lg px-2 py-1" placeholder="John Doe" />
         <label id="email" htmlFor="email" className="text-white">Email:</label>
         <input id="email" type="email" className="rounded-lg px-2 py-1" placeholder="Example@email.com" />
         <label id="message" htmlFor="message" className="text-white">Message:</label>
