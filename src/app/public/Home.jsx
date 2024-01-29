d07a0901f8ef5f4c9eba4ce6a1553819c16c2218
Arash Falah Parsa <arash_afp_94@hotmail.com>
2024-01-29 17:08:57 +0100

    adds dummy text to home page

--- src/app/public/Home.jsx
+++ src/app/public/Home.jsx
@@ -31,6 +31,13 @@ function Home() {
       </div>
       <div className="mx-auto font-normal mt-5 bg-blue-800 m-4 px-2 py-4 rounded-3xl w-3/4">
         <p className="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel aliquam provident ipsum et dolorem, tempore rerum sed dolores impedit cum at accusantium molestiae officiis odit, quaerat saepe possimus libero, pariatur assumenda sunt quae voluptate magnam eum. Sequi amet quo quasi exercitationem, dignissimos facilis eum esse voluptate! Cumque illo quae quo.</p>
+        <p className="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel aliquam provident ipsum et dolorem, tempore rerum sed dolores impedit cum at accusantium molestiae officiis odit, quaerat saepe possimus libero, pariatur assumenda sunt quae voluptate magnam eum. Sequi amet quo quasi exercitationem, dignissimos facilis eum esse voluptate! Cumque illo quae quo.</p>
+        <p className="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel aliquam provident ipsum et dolorem, tempore rerum sed dolores impedit cum at accusantium molestiae officiis odit, quaerat saepe possimus libero, pariatur assumenda sunt quae voluptate magnam eum. Sequi amet quo quasi exercitationem, dignissimos facilis eum esse voluptate! Cumque illo quae quo.</p>
+      </div>
+      <div className="mx-auto font-normal mt-5 bg-blue-800 m-4 px-2 py-4 rounded-3xl w-3/4">
+        <p className="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel aliquam provident ipsum et dolorem, tempore rerum sed dolores impedit cum at accusantium molestiae officiis odit, quaerat saepe possimus libero, pariatur assumenda sunt quae voluptate magnam eum. Sequi amet quo quasi exercitationem, dignissimos facilis eum esse voluptate! Cumque illo quae quo.</p>
+        <p className="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel aliquam provident ipsum et dolorem, tempore rerum sed dolores impedit cum at accusantium molestiae officiis odit, quaerat saepe possimus libero, pariatur assumenda sunt quae voluptate magnam eum. Sequi amet quo quasi exercitationem, dignissimos facilis eum esse voluptate! Cumque illo quae quo.</p>
+        <p className="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel aliquam provident ipsum et dolorem, tempore rerum sed dolores impedit cum at accusantium molestiae officiis odit, quaerat saepe possimus libero, pariatur assumenda sunt quae voluptate magnam eum. Sequi amet quo quasi exercitationem, dignissimos facilis eum esse voluptate! Cumque illo quae quo.</p>
       </div>
     </>
      
