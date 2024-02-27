df115e02c1fab9b8d007aaf9f199d39a0ee59b95
Arash Falah Parsa <arash_afp_94@hotmail.com>
2024-02-28 00:13:46 +0100

    Adds className prop to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
-function Footer() {
+function Footer({ className }) {
   const year = new Date().getFullYear();
   return (
-    <footer className="bg-blue-950 text-white p-4 text-center  w-full">
+    <footer className={`bg-blue-950 text-white p-4 text-center w-full ${className}`}>
       <p>© {year} Property of A.F.P. All rights reserved.</p>
       <p>
         Terms of Service | 
